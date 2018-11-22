d872de74edb423f518aebb201f6ed02fd74ea3e1
NEWGEN\arjun.singh <arjun.singh@newgen.co.in>
2018-11-22 11:52:36 +0530

    setting up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"/>
+     <br/>
+    <input type="password"
+      name="password"
+      v-model="password"
+      placeholder="passord"/>
+     <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'Arjun'
+    }, 2000)
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      // console.log('Register button is clicked', this.email + this.password)
+      console.log('Register response', response.data)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
